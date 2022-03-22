f1e3d663cefede1c169993e889d22d59e54532e2
whitew1994WW <whitew1994@gmail.com>
2022-03-22 11:32:27 +0000

    Changed version of tf to get tflite working, also added tests for string preprocessing

--- PyIng/ingredient_parser.py
+++ PyIng/ingredient_parser.py
@@ -1,3 +1,11 @@
+import PyIng
+import tflite_runtime.interpreter as tf_lite
+import numpy as np
+import re
+
+
+MODEL_PATH = "../models/output_model.tflite"
+
 
 def parse_ingredients(ingredients):
     """
@@ -19,29 +27,72 @@ def parse_ingredients(ingredients):
     pass
 
 
-def _build_tf_model():
+def _build_tf_interpreter():
     """
     Constructs a TF Lite model trained and saved from "train_model.ipynb".
     The TF lite model will then be able to take preprocessed input and convert it to a numeric output.
     :return:
     """
-    interpreter = tf.lite.Interpreter(TFLITE_FILE_PATH)
-    # There is only 1 signature defined in the model,
-    # so it will return it by default.
-    # If there are multiple signatures then we can pass the name.
-    my_signature = interpreter.get_signature_runner()
-
-    # my_signature is callable with input as arguments.
-    output = my_signature(x=tf.constant([1.0], shape=(1,10), dtype=tf.float32))
-    # 'output' is dictionary with all outputs from the inference.
-    # In this case we have single output 'result'.
-    print(output['result'])
+    interpreter = tf_lite.Interpreter(MODEL_PATH)
+    interpreter.allocate_tensors()
+    return interpreter
 
 
-def _preprocess_input_ingredients(ingredients):
-    return
+def _run_tf_interpreter(interpreter, input_data):
+    """
+    Runs inference on the tensorflow model trained in the ipynb in this project.
+
+    :param interpreter: Tensorflow interpreter created with _build_tf_interpreter
+    :param input_data: Input model data, already vectorized, length 60 array of integers
+    :return: Unit_name_output - [60, 2] float array, one row for unit and one for name.
+    Probabilities of word being a unit  or name respectively.
+    """
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    # The function `get_tensor()` returns a copy of the tensor data.
+    # Use `tensor()` in order to get a pointer to the tensor.
+    qty_output = interpreter.get_tensor(output_details[0]['index'])
+    unit_name_output = interpreter.get_tensor(output_details[1]['index'])
+
+    return unit_name_output, qty_output
+
+
+def _preprocess_input_ingredient_strings(ingredients: list):
+    processed_ings = []
+    for ing in ingredients:
+        processed_ings.append(_preprocess_input_ingredient_string(ing))
+    return processed_ings
+
+
+def _preprocess_input_ingredient_string(ingredient: str):
+    string = re.sub(r"([,./])", r" \g<1> ", ingredient)
+    string = re.sub(r'[^\w\s.,/]', r" ", string)
+    string = re.sub(r"\s+", r" ", string)
+    string = re.sub("(\s$)|(^\s)", "", string)
+
+    string = string.lower()
+    # Remove trailing white space
+    if string[-1] == " ":
+        string = string[:-1]
+    if string[0] == " ":
+        string = string[1:]
+    return string
 
 
 def _model_output_to_json(model_output, ingredients):
     return
 
+
+def _pad_sequences(input_ingredients):
+    return None
+
+
+def _vectorize_input_strings(input_ingredients):
+
+    return None
--- tests/test_ingredient_parser.py
+++ tests/test_ingredient_parser.py
@@ -1,14 +1,47 @@
 import unittest
+import PyIng
+import numpy as np
 
 
 class TestIngredientParser(unittest.TestCase):
     def test_parse_ingredients(self):
         self.assertEqual(True, True)
 
-    def test_build_tf_model(self):
-        pass
+    def test_build_tf_interreter(self):
+        interpreter = PyIng.ingredient_parser._build_tf_interpreter()
+        self.assertTrue(interpreter)
 
+    def test_run_tf_interpreter(self):
+        interpreter = PyIng.ingredient_parser._build_tf_interpreter()
+        input_details = interpreter.get_input_details()
+        # Test the model on random input data.
+        input_shape = input_details[0]['shape']
+        input_data = np.array(np.random.randint(1, 1000, input_shape), dtype=np.float32)
+        output_name, output_qty = PyIng.ingredient_parser._run_tf_interpreter(interpreter, input_data)
 
+        self.assertEqual(output_name.shape[1:], (2, 60))
+        self.assertEqual(output_qty.shape[1], 1)
 
+    def test_preprocess_input_ingredient_string(self):
+        input_ingredient = "3 1/2 Teaspoons of salt, (finely diced) "
+        expected_output = "3 1 / 2 teaspoons of salt , finely diced"
+        output = PyIng.ingredient_parser._preprocess_input_ingredient_string(input_ingredient)
+        self.assertEqual(output, expected_output)
+
+    def test_preprocess_input_ingredient_strings(self):
+        input_ingredient = ["3 1/2 Teaspoons of salt, (finely diced)"]*4
+        expected_output = ["3 1 / 2 teaspoons of salt , finely diced"]*4
+        output = PyIng.ingredient_parser._preprocess_input_ingredient_strings(input_ingredient)
+        self.assertListEqual(output, expected_output)
+
+    def test_vectorize_strings(self):
+        input_ingredients = ["3 1 / 2 teaspoons of salt , finely diced"]*4
+        output = PyIng.ingredient_parser._vectorize_input_strings(input_ingredients)
+        self.assertEqual(output.shape, (4, 10))
+
+    def test_pad_sequences(self):
+        input_ingredients = ["3 1 / 2 teaspoons of salt , finely diced"]*4
+        output = PyIng.ingredient_parser._pad_sequences(input_ingredients)
+        self.assertEqual(output.shape, (4, 60))
 if __name__ == '__main__':
     unittest.main()
