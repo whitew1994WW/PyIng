148fe9341ab4072bd3961303813db4e965a6d270
whitew1994WW <whitew1994@gmail.com>
2022-03-23 10:32:13 +0000

    Functionality for pre and post processing almost compelted. Unit tests completed also. Notebook refactored to include all embeddings needed.

--- PyIng/ingredient_parser.py
+++ PyIng/ingredient_parser.py
@@ -2,7 +2,7 @@ import PyIng
 import tflite_runtime.interpreter as tf_lite
 import numpy as np
 import re
-
+import json
 
 MODEL_PATH = "../models/output_model.tflite"
 
@@ -85,14 +85,74 @@ def _preprocess_input_ingredient_string(ingredient: str):
     return string
 
 
-def _model_output_to_json(model_output, ingredients):
+def _model_output_to_dict(model_name_unit_output, model_qty_output, ingredient: str):
+    """
+    Converts the tensorflow model output into the useful information
+    :param model_name_unit_output:
+    Numpy array of shape (2, 60) givin ght eprobabilities of each word being a unit or name
+    :param model_qty_output:
+    Float indicating the quantity outputted by the model
+    :param ingredient:
+    Processed ingredient string e.g. "1 ounce plain flour"
+    :return:
+    Dictionary of parts of input - {"name": "plain flour", "unit": "ounce", "qty", 1}
+    - Note the quantity is rounded to two decimal places
+    """
+    ingredient_list = np.array(ingredient.split(" "))
+    # convert the model output to a boolean mask for selecting the words from the input
+    name_mask = model_name_unit_output[0, :len(ingredient_list)].astype(np.bool)
+    unit_mask = model_name_unit_output[1, :len(ingredient_list)].astype(np.bool)
+    name = " ".join(ingredient_list[name_mask])
+    unit = " ".join(ingredient_list[unit_mask])
+    qty = round(model_qty_output, 2)
+    output_dict = {"name": name, "unit": unit, "qty": qty}
+    return output_dict
+
+
+def _model_output_to_list_of_dicts(model_name_unit_output, model_qty_output, ingredients):
     return
 
-
-def _pad_sequences(input_ingredients):
-    return None
-
-
 def _vectorize_input_strings(input_ingredients):
+    """
+    Converts parsed input list of ingredients to a list of index lists. Where each word is converted into a number
+    ["1 red apple", "1 red apple"] -> [[1, 120, 3], [1, 120, 3]]
+    :param input_ingredients: List of ingredients ["1 red apple", "1 red apple"]
+    :return: List of words numeric representations [[1, 120, 3], [1, 120, 3]]
+    """
+    word_index = _load_word_index()
+    output_ings = []
+    for ingredient in input_ingredients:
+        output_ings.append(_vectorize_input_string(ingredient, word_index))
+    return output_ings
 
-    return None
+
+def _vectorize_input_string(input_ingredient: str, word_index: dict):
+    """
+    Converts parsed input ingredient to a list of indexs.
+    Where each word is converted into a number
+
+    :param input_ingredients: Ingredient String e.g. "1 red apple"
+    :param word_index:
+    :return: List of words numeric representations [1, 120, 3]
+    """
+    # Convert to list of words
+    word_list = input_ingredient.split(" ")
+    idx_list = []
+
+    # Convert list to indexes - replace with OOV token if not in word list
+    for word in word_list:
+        if word in word_index:
+            idx_list.append(word_index[word])
+        else:
+            idx_list.append(word_index['<OOV>'])
+    return idx_list
+
+
+def _load_word_index():
+    """
+    Loads in the pre saved word index
+    :return: {"word": idx ..... }
+    """
+    with open("../word_index.json", "r") as f:
+        word_index = json.load(f)
+    return word_index
--- tests/test_ingredient_parser.py
+++ tests/test_ingredient_parser.py
@@ -1,6 +1,7 @@
 import unittest
 import PyIng
 import numpy as np
+import unittest.mock as mock
 
 
 class TestIngredientParser(unittest.TestCase):
@@ -34,14 +35,54 @@ class TestIngredientParser(unittest.TestCase):
         output = PyIng.ingredient_parser._preprocess_input_ingredient_strings(input_ingredient)
         self.assertListEqual(output, expected_output)
 
-    def test_vectorize_strings(self):
+    @mock.patch('PyIng.ingredient_parser._load_word_index')
+    def test_vectorize_strings(self, mock_word_index):
+        mock_word_index.return_value = {"<OOV>": 5, "3": 1, "of": 2, ",": 3}
         input_ingredients = ["3 1 / 2 teaspoons of salt , finely diced"]*4
         output = PyIng.ingredient_parser._vectorize_input_strings(input_ingredients)
-        self.assertEqual(output.shape, (4, 10))
+        print(output)
+        self.assertListEqual(output, [[1, 5, 5, 5, 5, 2, 5, 3, 5, 5]]*4)
 
-    def test_pad_sequences(self):
-        input_ingredients = ["3 1 / 2 teaspoons of salt , finely diced"]*4
-        output = PyIng.ingredient_parser._pad_sequences(input_ingredients)
-        self.assertEqual(output.shape, (4, 60))
+    def test_vectorize_string(self):
+        input_ingredients = "3 1 / 2 teaspoons of salt , finely diced"
+        word_index = {"<OOV>": 5, "3": 1, "of": 2, ",": 3}
+        output = PyIng.ingredient_parser._vectorize_input_string(input_ingredients, word_index)
+        print(output)
+        self.assertEqual(output, [1, 5, 5, 5, 5, 2, 5, 3, 5, 5])
+
+    def test_load_word_index(self):
+        word_index = PyIng.ingredient_parser._load_word_index()
+        self.assertIsInstance(word_index, dict)
+
+    def test_model_output_to_dict(self):
+        model_name_unit_output = np.zeros((2, 60), dtype=np.float64)
+        model_name_unit_output[0][2:4] = 0.6
+        model_name_unit_output[1][1] = 0.6
+        qty_output = 12.3452
+        input_parsed_ing = "1 ounce plain flour"
+        output_dict = PyIng.ingredient_parser._model_output_to_dict(model_name_unit_output, qty_output, input_parsed_ing)
+        expected_output = {
+            "name": "plain flour",
+            "unit": "ounce",
+            "qty": 12.35
+        }
+        self.assertEqual(output_dict, expected_output)
+
+    def test_model_output_to_list_of_dicts(self):
+        model_name_unit_outputs = np.zeros((3, 2, 60), dtype=np.float64)
+        model_name_unit_outputs[:][0][2:3] = 0.6
+        model_name_unit_outputs[:][1][1] = 0.6
+        qty_outputs = [12.3452]*3
+        input_parsed_ings = ["1 ounce plain flour"]*3
+        output_dict = PyIng.ingredient_parser._model_output_to_list_of_dicts(model_name_unit_outputs, qty_outputs, input_parsed_ings)
+        expected_output = 3 *[{
+            "name": "plain flour",
+            "unit": "ounce",
+            "qty": 12.35
+        }]
+        self.assertEqual(output_dict, expected_output)
+
+
+
 if __name__ == '__main__':
     unittest.main()
