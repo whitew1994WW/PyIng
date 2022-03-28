73e9212de100f3df70c1140401804616d220062d
whitew1994WW <whitew1994@gmail.com>
2022-03-28 12:35:34 +0100

    Added a python version, aded a deploy workflow in the CI, renamed the ipynb and wrote a readme. Updated setup.py

--- README.txt
+++
@@ -1,1 +0,0 @@
-https://www.kaggle.com/hugodarwood/epirecipes - recipe database
--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+import sys
+import os
+
+# circleci.py version
+VERSION = "1.1.1"
+
+def readme():
+    """print long description"""
+    with open('README.rst') as f:
+        return f.read()
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 
 setup(
     name='PyIng',
@@ -7,8 +33,11 @@ setup(
     description='Parses ingredient names into Name, Unit and Quantity',
     author='Will White',
     license='MIT',
-    install_requires=[],
-    setup_requires=[],
-    tests_require=[],
+    install_requires=["tflite-runtime>=2.5", "numpy"],
+    url='https://github.com/whitew1994WW/PyIng',
     test_suite='tests',
+    python_requires='>=3.7',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
